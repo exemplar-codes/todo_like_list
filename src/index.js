08a76aff47779afadab0bf2d5267a00692cb321d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-23 23:20:26 +0530

    Reset to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import AgeModal from "./components/Modals/AgeModal";
-import BlankParamModal from "./components/Modals/BlankParamModal";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AgeModal/>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
